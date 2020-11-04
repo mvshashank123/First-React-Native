09f6af558a37ed6b5f13dcd7f6e05d8d4c8392e6
Shashank <mv.shashank123@gmail.com>
2020-11-04 11:00:57 +0530

    Added the components

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import Todo from './Todo'
 
 export default function App() {
+  const [input,setInput] =useState('')
+  const [todos,setTodos] =useState([])
+
+  const addTodo = () =>{
+    setTodos([...todos,input])
+    setInput('')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Text style={styles.titleText}>Hello React Native</Text>
       <StatusBar style="auto" />
+      <ScrollView>
+      {todos.map((todo)=>(
+        <Todo title={todo}/>
+      ))}
+      </ScrollView>
+      
+      <TextInput placeholder="Enter the description" style={styles.textContainer} value ={input} onChangeText = {text =>setInput(text)}/>
+      <Button onPress={addTodo} title="Add Todo"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 35
   },
+  titleText: {
+    //backgroundColor:'green',
+    fontSize: 30,
+    fontWeight:'bold'
+  },
+  textContainer: {
+    margin : 20,
+    height: 40,
+    borderColor: 'red',
+    borderWidth: 1
+  }
 });
---
+++ Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Todo = ({title = "Work"}) => {
+    return (
+        <View>
+            <Text>{title}</Text>
+        </View>
+    )
+}
+
+export default Todo
+
+const styles = StyleSheet.create({})
