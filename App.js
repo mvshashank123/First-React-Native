d6666058ec8c35d91901464305c51c095cd38216
Shashank <mv.shashank123@gmail.com>
2020-11-06 11:43:08 +0530

    Changes

--- App.js
+++ App.js
@@ -1,46 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
-import Todo from './Todo'
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+import Todo from "./Todo";
 
 export default function App() {
-  const [input,setInput] =useState('')
-  const [todos,setTodos] =useState([])
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [total, setTotal] = useState(0);
+  const [expenses, setExpenses] = useState([
+    {
+      description: "Book",
+      amount: 200
+    },
+  ]);
 
-  const addTodo = () =>{
-    setTodos([...todos,input])
-    setInput('')
-  }
+  useEffect(() =>{
+    setTotal(expenses.reduce((total,expense) => total+expense.amount,0))
+  },[expenses])
+
+  const addExpenses = () => {
+    setExpenses([...expenses, { description: description, amount: amount }]);
+    setAmount('')
+    setDescription('')
+  };
   return (
     <View style={styles.container}>
-      
       <Text style={styles.titleText}>Hello React Native</Text>
+      <Text>Total: {total}</Text>
       <StatusBar style="auto" />
+
+      <TextInput
+        placeholder="Enter the description"
+        style={styles.textContainer}
+        value={description}
+        onChangeText={(text) => setDescription(text)}
+      />
+      <TextInput
+        placeholder="Enter the amount"
+        style={styles.textContainer}
+        value={amount}
+        onChangeText={(text) => setAmount(text)}
+      />
+      <Button onPress={addExpenses} title="Add Todo" />
       <ScrollView>
-      {todos.map((todo)=>(
-        <Todo title={todo}/>
-      ))}
+        {expenses.map((expense)=>(
+          <View>
+          <Text>{expense.description}</Text>
+          <Text>{expense.amount}</Text>
+          </View>
+        ))}
       </ScrollView>
-      
-      <TextInput placeholder="Enter the description" style={styles.textContainer} value ={input} onChangeText = {text =>setInput(text)}/>
-      <Button onPress={addTodo} title="Add Todo"/>
+      <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 35
+    marginTop: 35,
   },
   titleText: {
     //backgroundColor:'green',
     fontSize: 30,
-    fontWeight:'bold'
+    fontWeight: "bold",
   },
   textContainer: {
-    margin : 20,
+    margin: 20,
     height: 40,
-    borderColor: 'red',
-    borderWidth: 1
-  }
+    borderColor: "red",
+    borderWidth: 1,
+  },
 });
